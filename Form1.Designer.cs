b6069c277e86097d6b2add927b378168ed3f2030
hsunami21 <whsu21@hotmail.com>
2015-04-20 12:52:28 -0400

    Salary Tracker
    Completed using Windows Form Application in Visual Studio 2012

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WendallHsu_Test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmpNo = new System.Windows.Forms.Label();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.txtEmpNo = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDeleteOne = new System.Windows.Forms.Button();
+            this.btnDeleteAll = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblEmpNo
+            // 
+            this.lblEmpNo.AutoSize = true;
+            this.lblEmpNo.Location = new System.Drawing.Point(29, 41);
+            this.lblEmpNo.Name = "lblEmpNo";
+            this.lblEmpNo.Size = new System.Drawing.Size(118, 13);
+            this.lblEmpNo.TabIndex = 0;
+            this.lblEmpNo.Text = "EMPLOYEE NUMBER:";
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.Location = new System.Drawing.Point(29, 74);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(108, 13);
+            this.lblSalary.TabIndex = 1;
+            this.lblSalary.Text = "MONTHLY SALARY:";
+            // 
+            // txtEmpNo
+            // 
+            this.txtEmpNo.Location = new System.Drawing.Point(153, 38);
+            this.txtEmpNo.Name = "txtEmpNo";
+            this.txtEmpNo.Size = new System.Drawing.Size(154, 20);
+            this.txtEmpNo.TabIndex = 2;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(153, 71);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(154, 20);
+            this.txtSalary.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(337, 35);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(114, 52);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "ADD EMP NO and SALARY";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDeleteOne
+            // 
+            this.btnDeleteOne.Location = new System.Drawing.Point(337, 93);
+            this.btnDeleteOne.Name = "btnDeleteOne";
+            this.btnDeleteOne.Size = new System.Drawing.Size(114, 52);
+            this.btnDeleteOne.TabIndex = 5;
+            this.btnDeleteOne.Text = "DELETE ONE RECORD";
+            this.btnDeleteOne.UseVisualStyleBackColor = true;
+            this.btnDeleteOne.Click += new System.EventHandler(this.btnDeleteOne_Click);
+            // 
+            // btnDeleteAll
+            // 
+            this.btnDeleteAll.Location = new System.Drawing.Point(337, 151);
+            this.btnDeleteAll.Name = "btnDeleteAll";
+            this.btnDeleteAll.Size = new System.Drawing.Size(114, 52);
+            this.btnDeleteAll.TabIndex = 6;
+            this.btnDeleteAll.Text = "DELETE ALL RECORDS";
+            this.btnDeleteAll.UseVisualStyleBackColor = true;
+            this.btnDeleteAll.Click += new System.EventHandler(this.btnDeleteAll_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(337, 209);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(114, 52);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(337, 267);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(114, 52);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "CLEAR DISPLAY SCREEN";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(337, 325);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(114, 52);
+            this.btnDisplay.TabIndex = 9;
+            this.btnDisplay.Text = "DISPLAY ALL RECORDS";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(337, 383);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(114, 52);
+            this.btnQuit.TabIndex = 10;
+            this.btnQuit.Text = "QUIT";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(32, 110);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ReadOnly = true;
+            this.txtDisplay.Size = new System.Drawing.Size(275, 325);
+            this.txtDisplay.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 497);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDeleteAll);
+            this.Controls.Add(this.btnDeleteOne);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtEmpNo);
+            this.Controls.Add(this.lblSalary);
+            this.Controls.Add(this.lblEmpNo);
+            this.Name = "Form1";
+            this.Text = "Hsu Co. Ltd: Employee Salary Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmpNo;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.TextBox txtEmpNo;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDeleteOne;
+        private System.Windows.Forms.Button btnDeleteAll;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.TextBox txtDisplay;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WendallHsu_Test1
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string empNo = txtEmpNo.Text;
+            string salary = txtSalary.Text;
+
+            if (empNo == "" || salary == "")
+            {
+                MessageBox.Show("Please enter an employee number and the respective salary to add.");
+            }
+            else
+            {
+                myDictionary.Add(empNo, int.Parse(salary));
+                MessageBox.Show("Successfully added.");
+                txtDisplay.Text = "Employee Number: " + empNo + "   Salary: " + salary;
+            }
+        }
+
+        private void btnDeleteOne_Click(object sender, EventArgs e)
+        {
+            string empNo = txtEmpNo.Text;
+
+            if (empNo == "")
+            {
+                MessageBox.Show("Please enter an employee number to delete.");
+            }
+            else if (myDictionary.ContainsKey(empNo))
+            {
+                myDictionary.Remove(empNo);
+                MessageBox.Show("Successfully removed.");
+                txtDisplay.Text = "";
+            }
+            else if (myDictionary.Count == 0)
+            {
+                MessageBox.Show("There are no records in the dictionary to be deleted.");
+            }
+            else
+            {
+                MessageBox.Show("That employee number does not exist and cannot be deleted.");
+            }
+        }
+
+        private void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            if (myDictionary.Count == 0)
+            {
+                MessageBox.Show("There are no records in the dictionary to be deleted.");
+            }
+            else
+            {
+                myDictionary.Clear();
+                MessageBox.Show("Successfully removed all records.");
+                txtDisplay.Text = "";
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string empNo = txtEmpNo.Text;
+
+            if (empNo == "")
+            {
+                MessageBox.Show("Please enter an employee number to search.");
+            }
+            else if (myDictionary.ContainsKey(empNo))
+            {
+                int value = myDictionary[empNo];
+                txtDisplay.Text = "Employee Number: " + empNo + "   Salary: " + value;
+            }
+                else if (myDictionary.Count == 0)
+            {
+                MessageBox.Show("There are no records in the dictionary.");
+            }
+            else
+            {
+                MessageBox.Show("No such employee number exists.");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "")
+            {
+                MessageBox.Show("There is nothing on the display to clear.");
+            }
+            else
+            {
+                txtDisplay.Text = "";
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+
+            if (myDictionary.Count == 0)
+            {
+                MessageBox.Show("There are no records in the dictionary to be shown.");
+            }
+            else
+            {
+                foreach (KeyValuePair<string, int> pair in myDictionary)
+                {
+                    txtDisplay.Text += "Employee Number: " + pair.Key + "   Salary: " + pair.Value + "\r\n";
+                }
+            }
+           
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye.");
+            Application.Exit();
+        }
+       
+    }
+}
